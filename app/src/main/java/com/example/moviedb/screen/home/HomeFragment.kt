83970744eb76dee1ab0cefce2793ea1b280b16c0
Duc Anh <ducanh@mlemmlem.local>
2020-01-30 10:57:39 +0700

    hidden key

--- app/src/main/java/com/example/moviedb/screen/home/HomeFragment.kt
+++ app/src/main/java/com/example/moviedb/screen/home/HomeFragment.kt
@@ -4,8 +4,10 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import com.example.moviedb.BuildConfig
 import com.example.moviedb.R
 import com.example.moviedb.base.BaseFragment
+import java.util.*
 
 class HomeFragment: BaseFragment() {
 
@@ -15,5 +17,10 @@ class HomeFragment: BaseFragment() {
         savedInstanceState: Bundle?
     ): View? {
         return inflater.inflate(R.layout.fragment_home, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        BuildConfig.API_KEY
     }
 }
